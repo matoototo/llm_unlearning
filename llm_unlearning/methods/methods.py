adce716caa6b694a1c3ca65607f8540a120c7f3b
Mato Gudelj <mato.gudelj@tum.de>
2024-07-15 14:20:48 +0200

    Remove superfluous setup() method in Method.

--- llm_unlearning/methods/methods.py
+++ llm_unlearning/methods/methods.py
@@ -14,11 +14,7 @@ def check_inputs(required_inputs: List[str], **kwargs):
 
 class Method:
     def __init__(self, **kwargs):
-        self.setup(**kwargs)
         self.input_keys = ["input_ids", "attention_mask", "labels"]
-
-    def setup(self, **kwargs):
-        pass
 
     def compute_loss(self, model: PreTrainedModel, **kwargs) -> Tuple[torch.Tensor, Dict[str, float], Any]:
         raise NotImplementedError("Subclasses must implement this method")
